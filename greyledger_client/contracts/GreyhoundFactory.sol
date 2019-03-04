21c41d8b10510ebef61933ffb22b99858f1c7088
atkinsonholly <atkinsonholly@gmail.com>
2019-03-04 11:36:02 +0000

    Tested version of GreyhoundFactory added

--- greyledger_client/contracts/GreyhoundFactory.sol
+++ greyledger_client/contracts/GreyhoundFactory.sol
@@ -1,51 +1,51 @@
-pragma solidity ^0.5.4;
+pragma solidity >=0.4.21 <0.6.0;
 
 contract greyhoundFactory {
 
-    uint numGreyhounds;
+    uint numGreyhounds = 0;
     uint greyhoundRef = 16;
     uint refModulus = 10 ** greyhoundRef;
+    uint[] myGreyhounds;
 
     struct Greyhound {
-        mapping (uint => string) name;
-        mapping (uint => string) ear_marks;
-        string sex;
-        string sire;
-        uint birthdate;
-        string status;
-        string owners;
-        uint ref;
-        mapping (uint => address) user;
+        string name; // greyhound name - can be updated
+        string ear_marks; // greyhound ear_marks - can be updated (left_ear)
+        string sexSireBirthdate; // these fields won't be updated by update function
+        string status; // e.g. initial registration, new owner(s), retired, euthanised, natural death
+        string owners; // string containing all owners and addresses
+        uint ref; // pseudo-random number
+        address[] users; // initialise an empty array of user addresses
     }
 
     Greyhound[] public greyhounds; // all greyhounds ever created
 
-    event GreyhoundAdded(
-        uint greyhoundId);
+    event GreyhoundAdded(uint greyhoundId, string name);
 
     function getNumGreyhounds() public view returns (uint) {
         return numGreyhounds;
     }
 
     function _createGreyhound(
-        string memory _sex,
-        string memory _sire,
-        uint _birthdate,
+        string memory _name,
+        string memory _ear_marks,
+        string memory _sexSireBirthdate,
         string memory _status,
         string memory _owners,
-        uint _randRef)
+        uint _randRef,
+        address[] memory _address)
         private returns(uint) {
             numGreyhounds++;
             Greyhound memory greyhound = Greyhound(
-                _sex,
-                _sire,
-                _birthdate,
+                _name,
+                _ear_marks,
+                _sexSireBirthdate,
                 _status,
                 _owners,
-                _randRef
+                _randRef,
+                _address
             );
             uint index = greyhounds.push(greyhound) - 1;
-            emit GreyhoundAdded(index);
+            emit GreyhoundAdded(index, _name);
             return index;
     }
 
@@ -59,36 +59,38 @@ contract greyhoundFactory {
     function createUniqueGreyhound(
         string memory _name,
         string memory _ear_marks,
-        string memory _sex,
-        string memory _sire,
-        uint _birthdate,
+        string memory _sexSireBirthdate,
         string memory _status,
-        string memory _owners
+        string memory _owners,
+        address[] memory _address
         ) public returns(uint) {
         uint randRef = _generateRandomRef(_name);
         uint greyhoundId = _createGreyhound(
-            _sex,
-            _sire,
-            _birthdate,
+            _name,
+            _ear_marks,
+            _sexSireBirthdate,
             _status,
             _owners,
-            randRef
+            randRef,
+            _address
             );
         Greyhound storage g = greyhounds[greyhoundId];
-        g.name[greyhoundId] = _name;
-        g.ear_marks[greyhoundId] = _ear_marks;
-        g.user[greyhoundId] = msg.sender;
+        g.users.push(msg.sender);
         return greyhoundId;
     }
 
-    function findMyGreyhounds(address _address) public view returns(uint[] memory) {
-        uint[] memory myGreyhounds;
+    function findMyGreyhounds() public returns(uint[] memory) {
+        uint[] memory tempGreyhoundList;
+        myGreyhounds = tempGreyhoundList;
         uint j = 0;
         for(uint i = 0; i<greyhounds.length; i++){
             Greyhound storage g = greyhounds[i];
-            if(g.user[i] == _address){
-                myGreyhounds[j] = g.ref;
-                j++;
+            for(uint k = 0; k<g.users.length; k++){
+                address a = g.users[k];
+                if(a == msg.sender){
+                    myGreyhounds.push(g.ref);
+                    j++;
+                }
             }
         }
         // return user's list of unique greyhound identifiers
@@ -103,15 +105,30 @@ contract greyhoundFactory {
         string memory _owners) public returns(string memory) {
             for(uint i = 0; i<greyhounds.length; i++){
                 Greyhound storage g = greyhounds[i];
-                if(keccak256(abi.encodePacked(g.name[i])) == keccak256(abi.encodePacked(_name))){
-                    g.name[i] = _new_name;
+                // if greyhound found, update details
+                if(keccak256(abi.encodePacked(g.name)) == keccak256(abi.encodePacked(_name))){
+                    g.name = _new_name;
                     g.status = _status;
-                    g.ear_marks[i] = _ear_marks;
+                    g.ear_marks = _ear_marks;
                     g.owners = _owners;
-                    g.user[i] = msg.sender;
-                    return g.name[i];
+
+                    // only add user if new address
+                    bool userCheck = false;
+                    for(uint k = 0; k<g.users.length; k++){
+                        address a = g.users[k];
+                        if(a == msg.sender){
+                            userCheck = true;
+                        }
+                    }
+                    if(userCheck == false){
+                        g.users.push(msg.sender);
+                    }
+
+                    // return new name
+                    return g.name;
                 }
             }
+        // if greyhound name not found, return message
         return "Unsuccessful request";
     }
 
