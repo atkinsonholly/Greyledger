94c452055a1043bf4ea29f501388d34ba41cc47a
atkinsonholly <atkinsonholly@gmail.com>
2019-03-06 11:58:39 +0000

    Form styling

--- greyledger_client/app/src/components/forms/NewGreyhoundForm.js
+++ greyledger_client/app/src/components/forms/NewGreyhoundForm.js
@@ -5,116 +5,114 @@ const NewGreyhoundForm = props => {
   return (
     <div className="form-container">
       <h2>Greyhound Registration</h2>
-      <form onSubmit={props.saveGreyhoundToDB}>
-        <h3 className="greyhound_form_header">Greyhound Information</h3>
-        <div className="row">
-          <div className="col-1"><label>Greyhound Racing Name:</label></div>
-          <div className="col-2"><input type="text" name="name" placeholder="Max. 16 characters" onChange={props.handleChange}/></div>
-        </div>
-        <div className="row">
-          <div className="col-1"><label>Earmarks (left):</label></div>
-          <div className="col-2"><input type="text" name="left_ear" placeholder="Enter adult ear mark" onChange={props.handleChange}/></div>
-        </div>
-        <div className="row">
-          <div className="col-1"><label>Earmarks (right):</label></div>
-          <div className="col-2"><input type="text" name="right_ear" placeholder="Enter litter ear mark" onChange={props.handleChange}/></div>
-        </div>
-        <div className="row">
-          <div className="col-1"><label>Sire:</label></div>
-          <div className="col-2"><input type="text" name="sire" placeholder="Enter sire" onChange={props.handleChange}/></div>
-        </div>
-        <div className="row">
-          <div className="col-1"><label>Sex:</label></div>
-          <div className="col-2"><select name="sex" onChange={props.handleChange}>
-            <option value="M">Male</option>
-            <option value="F">Female</option>
-          </select></div>
-        </div>
-        <div className="row">
-          <div className="col-1"><label>Enter greyhound's date of birth:</label></div>
-          <div className="col-2"><input type="date" name="birthdate" onChange={props.handleChange}/></div>
-        </div>
-        <h3 className="vaccine_header">Enter dates of latest vaccines</h3>
-        <div className="row">
-          <div className="col-1"><label>Distemper:</label></div>
-          <div className="col-2"><input type="date" name="distemper" onChange={props.handleChange}/></div>
-        </div>
-        <div className="row">
-          <div className="col-1"><label>Viral hepatitis:</label></div>
-          <div className="col-2"><input type="date" name="viral_hepatitis" onChange={props.handleChange}/></div>
-        </div>
-        <div className="row">
-          <label>Leptospira canicola:</label>
-          <input type="date" name="leptospira_canicola" onChange={props.handleChange}/>
-        </div>
-        <div className="row">
-          <label>Leptospira icterihaemorrhagiae:</label>
-          <input type="date" name="leptospira_icterihaemorrhagiae" onChange={props.handleChange}/>
-        </div>
-        <div className="row">
-          <label>Parvovirus:</label>
-            <input type="date" name="parvovirus" onChange={props.handleChange}/>
+        <form onSubmit={props.saveGreyhoundToDB}>
+          <h3 className="greyhound_form_header">Greyhound Information</h3>
+          <div className="row">
+            <div className="col-1"><label>Greyhound Racing Name:</label></div>
+            <div className="col-2"><input type="text" name="name" placeholder="Max. 16 characters" onChange={props.handleChange}/></div>
+          </div>
+          <div className="row">
+            <div className="col-1"><label>Earmarks (left):</label></div>
+            <div className="col-2"><input type="text" name="left_ear" placeholder="Enter adult ear mark" onChange={props.handleChange}/></div>
+          </div>
+          <div className="row">
+            <div className="col-1"><label>Earmarks (right):</label></div>
+            <div className="col-2"><input type="text" name="right_ear" placeholder="Enter litter ear mark" onChange={props.handleChange}/></div>
+          </div>
+          <div className="row">
+            <div className="col-1"><label>Sire:</label></div>
+            <div className="col-2"><input type="text" name="sire" placeholder="Enter sire" onChange={props.handleChange}/></div>
+          </div>
+          <div className="row">
+            <div className="col-1"><label>Sex:</label></div>
+            <div className="col-2"><select name="sex" onChange={props.handleChange}>
+              <option value="M">Male</option>
+              <option value="F">Female</option>
+            </select></div>
+          </div>
+          <div className="row">
+            <div className="col-1"><label>Enter greyhound's date of birth:</label></div>
+            <div className="col-2"><input type="date" name="birthdate" onChange={props.handleChange}/></div>
+          </div>
+          <h3 className="vaccine_header">Enter dates of latest vaccines</h3>
+          <div className="row">
+            <div className="col-1"><label>Distemper:</label></div>
+            <div className="col-2"><input type="date" name="distemper" onChange={props.handleChange}/></div>
+          </div>
+          <div className="row">
+            <div className="col-1"><label>Viral hepatitis:</label></div>
+            <div className="col-2"><input type="date" name="viral_hepatitis" onChange={props.handleChange}/></div>
+          </div>
+          <div className="row">
+            <div className="col-1"><label>Leptospira canicola:</label></div>
+            <div className="col-2"><input type="date" name="leptospira_canicola" onChange={props.handleChange}/></div>
+          </div>
+          <div className="row">
+            <div className="col-1"><label>Leptospira icterihaemorrhagiae:</label></div>
+            <div className="col-2"><input type="date" name="leptospira_icterihaemorrhagiae" onChange={props.handleChange}/></div>
+          </div>
+          <div className="row">
+            <div className="col-1"><label>Parvovirus:</label></div>
+            <div className="col-2"><input type="date" name="parvovirus" onChange={props.handleChange}/></div>
+          </div>
 
-        </div>
-        <div className="row">
           <h3>Owner 1</h3>
-          <div>
-            <label>First Name</label>
-            <input type="text" name="owner_1_first_name" onChange={props.handleChange}/>
+          <div className="row">
+            <div className="col-1"><label>First Name</label></div>
+            <div className="col-2"><input type="text" name="owner_1_first_name" onChange={props.handleChange}/></div>
           </div>
           <div className="row">
-            <label>Last Name</label>
-            <input type="text" name="owner_1_last_name" onChange={props.handleChange}/>
+            <div className="col-1"><label>Last Name</label></div>
+            <div className="col-2"><input type="text" name="owner_1_last_name" onChange={props.handleChange}/></div>
           </div>
           <div className="row">
-            <label>Address</label>
-            <input type="text" name="owner_1_address" onChange={props.handleChange}/>
+            <div className="col-1"><label>Address</label></div>
+            <div className="col-2"><input type="text" name="owner_1_address" onChange={props.handleChange}/></div>
           </div>
           <h3>Owner 2</h3>
           <div className="row">
-            <label>First Name</label>
-            <input type="text" name="owner_2_first_name" onChange={props.handleChange}/>
+            <div className="col-1"><label>First Name</label></div>
+            <div className="col-2"><input type="text" name="owner_2_first_name" onChange={props.handleChange}/></div>
           </div>
           <div className="row">
-            <label>Last Name</label>
-            <input type="text" name="owner_2_last_name" onChange={props.handleChange}/>
+            <div className="col-1"><label>Last Name</label></div>
+            <div className="col-2"><input type="text" name="owner_2_last_name" onChange={props.handleChange}/></div>
           </div>
           <div className="row">
-            <label>Address</label>
-            <input type="text" name="owner_2_address" onChange={props.handleChange}/>
+            <div className="col-1"><label>Address</label></div>
+            <div className="col-2"><input type="text" name="owner_2_address" onChange={props.handleChange}/></div>
           </div>
           <h3>Owner 3</h3>
           <div className="row">
-            <label>First Name</label>
-            <input type="text" name="owner_3_first_name" onChange={props.handleChange}/>
+            <div className="col-1"><label>First Name</label></div>
+            <div className="col-2"><input type="text" name="owner_3_first_name" onChange={props.handleChange}/></div>
           </div>
           <div className="row">
-            <label>Last Name</label>
-            <input type="text" name="owner_3_last_name" onChange={props.handleChange}/>
+            <div className="col-1"><label>Last Name</label></div>
+            <div className="col-2"><input type="text" name="owner_3_last_name" onChange={props.handleChange}/></div>
           </div>
           <div className="row">
-            <label>Address</label>
-            <input type="text" name="owner_3_address" onChange={props.handleChange}/>
+            <div className="col-1"><label>Address</label></div>
+            <div className="col-2"><input type="text" name="owner_3_address" onChange={props.handleChange}/></div>
           </div>
           <h3>Owner 4</h3>
           <div className="row">
-            <label>First Name</label>
-            <input type="text" name="owner_4_first_name" onChange={props.handleChange}/>
+            <div className="col-1"><label>First Name</label></div>
+            <div className="col-2"><input type="text" name="owner_4_first_name" onChange={props.handleChange}/></div>
           </div>
           <div className="row">
-            <label>Last Name</label>
-            <input type="text" name="owner_4_last_name" onChange={props.handleChange}/>
+            <div className="col-1"><label>Last Name</label></div>
+            <div className="col-2"><input type="text" name="owner_4_last_name" onChange={props.handleChange}/></div>
           </div>
           <div className="row">
-            <label>Address</label>
-            <input type="text" name="owner_4_address" onChange={props.handleChange}/>
+            <div className="col-1"><label>Address</label></div>
+            <div className="col-2"><input type="text" name="owner_4_address" onChange={props.handleChange}/></div>
           </div>
-        </div>
-        <div className="row">
-          <label>Accept Terms and Conditions:</label>
-          <input type="checkbox" name="isAccepted" checked={props.isAccepted} onChange={props.handleChange} />
-        </div>
-        <input className="submit-button" type="submit" value="Submit this greyhound" />
+          <div className="row accept">
+            <div className="col-1"><label>Accept Terms and Conditions:</label></div>
+            <div className="col-2"><input className="check-box" type="checkbox" name="isAccepted" checked={props.isAccepted} onChange={props.handleChange} /></div>
+          </div>
+        <div><input className="submit-button" type="submit" value="Submit this greyhound" /></div>
       </form>
     </div>
   )
--- greyledger_client/app/src/components/forms/UpdateGreyhoundForm.js
+++ greyledger_client/app/src/components/forms/UpdateGreyhoundForm.js
@@ -2,150 +2,122 @@ import React from "react";
 
 const UpdateGreyhoundForm = props => {
   return (
-    <div>
-    <h2>Change of Registered Ownership, or Change of Greyhound's name</h2>
-    <form onSubmit={props.sendUpdateToDB}>
-      <h3 className="greyhound_form_header">Greyhound Information</h3>
-      <div>
-        Status:
-        <select name="status" onChange={props.handleChange}>
-          <option name="nameChange" value="Greyhound has a new name">Greyhound has a new name</option>
-          <option name="retired" value="Greyhound has retired">Greyhound has retired</option>
-          <option name="changeOfOwnership" value="Notification of new ownership">Notification of new owners</option>
-          <option name="euthanised" value="Greyhound has been euthanised">Greyhound has been euthanised</option>
-          <option name="naturalDeath" value="Death by natural causes">Death by natural causes</option>
-        </select>
-      </div>
-      {props.status === "Greyhound has been euthanised" || props.status === "Death by natural causes" ?
-      <div>
-        <div>
-          <label>
-            Enter date of death:
-            <input type="date" name="date_of_death" onChange={props.handleChange}/>
-          </label>
+    <div className="form-container">
+      <h2>Change of Registered Ownership, or Change of Greyhound's Name</h2>
+      <form onSubmit={props.sendUpdateToDB}>
+        <h3 className="greyhound_form_header">Greyhound Information</h3>
+        <div className="row">
+          <div className="col-1"><label>Status:</label></div>
+          <div className="col-2">
+            <select name="status" onChange={props.handleChange}>
+              <option name="nameChange" value="Greyhound has a new name">Greyhound has a new name</option>
+              <option name="retired" value="Greyhound has retired">Greyhound has retired</option>
+              <option name="changeOfOwnership" value="Notification of new ownership">Notification of new owners</option>
+              <option name="euthanised" value="Greyhound has been euthanised">Greyhound has been euthanised</option>
+              <option name="naturalDeath" value="Death by natural causes">Death by natural causes</option>
+            </select>
+          </div>
         </div>
+        {props.status === "Greyhound has been euthanised" || props.status === "Death by natural causes" ?
         <div>
-          <label>
-            Enter vet information or other details of the death:
-            <input type="text" name="details_of_death" onChange={props.handleChange}/>
-          </label>
+          <div className="row">
+            <div className="col-1"><label>Enter date of death:</label></div>
+            <div className="col-2"><input type="date" name="date_of_death" onChange={props.handleChange}/></div>
+          </div>
+          <div className="row">
+            <div className="col-1"><label>Enter vet information or other details of the death:</label></div>
+            <div className="col-2"><input type="text" name="details_of_death" onChange={props.handleChange}/></div>
+          </div>
         </div>
-      </div>
-      : null}
+        : null}
 
-      {props.status === "Greyhound has a new name" ?
-      <div>
+        {props.status === "Greyhound has a new name" ?
         <div>
-          <label>
-            Enter greyhound's previous name (MANDATORY):
-            <input type="text" name="previous_name" placeholder="Max. 16 characters" onChange={props.handleChange}/>
-          </label>
+          <div className="row">
+            <div className="col-1"><label>Enter greyhound's previous name:</label></div>
+            <div className="col-2"><input type="text" name="previous_name" placeholder="Max. 16 characters" onChange={props.handleChange}/></div>
+          </div>
+          <div className="row">
+            <div className="col-1"><label>Enter greyhound's new name:</label></div>
+            <div className="col-2"><input type="text" name="new_name" placeholder="Only include if the name has changed" onChange={props.handleChange}/></div>
+          </div>
         </div>
-        <div>
-          <label>
-            Enter greyhound's new name:
-            <input type="text" name="new_name" placeholder="Only include if the name has changed" onChange={props.handleChange}/>
-          </label>
+        :
+        <div className="row">
+          <div className="col-1"><label>Enter greyhound's name:</label></div>
+          <div className="col-2"><input type="text" name="previous_name" placeholder="Max. 16 characters" onChange={props.handleChange}/></div>
         </div>
-      </div>
-      :
-      <div>
-        <label>
-          Enter greyhound's name:
-          <input type="text" name="previous_name" placeholder="Max. 16 characters" onChange={props.handleChange}/>
-        </label>
-      </div>
-      }
+        }
 
-      <div>
-        <label>
-          Earmarks (right):
-          <input type="text" name="right_ear" placeholder="Enter litter ear mark" onChange={props.handleChange}/>
-        </label>
-      </div>
-      <div>
-        <label>
-          Earmarks (left):
-          <input type="text" name="left_ear" placeholder="Enter adult ear mark" onChange={props.handleChange}/>
-        </label>
-      </div>
+        <div className="row">
+          <div className="col-1"><label>Earmarks (right):</label></div>
+          <div className="col-2"><input type="text" name="right_ear" placeholder="Enter litter ear mark" onChange={props.handleChange}/></div>
+        </div>
+        <div className="row">
+          <div className="col-1"><label>Earmarks (left):</label></div>
+          <div className="col-2"><input type="text" name="left_ear" placeholder="Enter adult ear mark" onChange={props.handleChange}/></div>
+        </div>
 
-      <div>
-        <h3>Please enter the names and addresses of all current owners below</h3>
-        <h3>Owner 1</h3>
-        <div>
-          <label> First Name
-            <input type="text" name="owner_1_first_name" onChange={props.handleChange}/>
-          </label>
-        </div>
-        <div>
-          <label> Last Name
-            <input type="text" name="owner_1_last_name" onChange={props.handleChange}/>
-          </label>
-        </div>
-        <div>
-          <label> Address
-            <input type="text" name="owner_1_address" onChange={props.handleChange}/>
-          </label>
-        </div>
-        <h3>Owner 2</h3>
-        <div>
-          <label> First Name
-            <input type="text" name="owner_2_first_name" onChange={props.handleChange}/>
-          </label>
-        </div>
-        <div>
-          <label> Last Name
-            <input type="text" name="owner_2_last_name" onChange={props.handleChange}/>
-          </label>
-        </div>
-        <div>
-          <label> Address
-            <input type="text" name="owner_2_address" onChange={props.handleChange}/>
-          </label>
-        </div>
-        <h3>Owner 3</h3>
-        <div>
-          <label> First Name
-            <input type="text" name="owner_3_first_name" onChange={props.handleChange}/>
-          </label>
-        </div>
-        <div>
-          <label> Last Name
-            <input type="text" name="owner_3_last_name" onChange={props.handleChange}/>
-          </label>
-        </div>
-        <div>
-          <label> Address
-            <input type="text" name="owner_3_address" onChange={props.handleChange}/>
-          </label>
-        </div>
-        <h3>Owner 4</h3>
-        <div>
-          <label> First Name
-            <input type="text" name="owner_4_first_name" onChange={props.handleChange}/>
-          </label>
-        </div>
-        <div>
-          <label> Last Name
-            <input type="text" name="owner_4_last_name" onChange={props.handleChange}/>
-          </label>
-        </div>
-        <div>
-          <label> Address
-            <input type="text" name="owner_4_address" onChange={props.handleChange}/>
-          </label>
-        </div>
-      </div>
-      <div>
-        <label>
-          Accept Terms and Conditions:
-          <input type="checkbox" name="isAccepted" checked={props.isAccepted} onChange={props.handleChange} />
-        </label>
-      </div>
-      <input className="submit-button" type="submit" value="Update this greyhound" />
-    </form>
+          <h3>Please enter the names and addresses of all current owners below</h3>
+            <h3>Owner 1</h3>
+            <div className="row">
+              <div className="col-1"><label>First Name</label></div>
+              <div className="col-2"><input type="text" name="owner_1_first_name" onChange={props.handleChange}/></div>
+            </div>
+            <div className="row">
+              <div className="col-1"><label>Last Name</label></div>
+              <div className="col-2"><input type="text" name="owner_1_last_name" onChange={props.handleChange}/></div>
+            </div>
+            <div className="row">
+              <div className="col-1"><label>Address</label></div>
+              <div className="col-2"><input type="text" name="owner_1_address" onChange={props.handleChange}/></div>
+            </div>
+            <h3>Owner 2</h3>
+            <div className="row">
+              <div className="col-1"><label>First Name</label></div>
+              <div className="col-2"><input type="text" name="owner_2_first_name" onChange={props.handleChange}/></div>
+            </div>
+            <div className="row">
+              <div className="col-1"><label>Last Name</label></div>
+              <div className="col-2"><input type="text" name="owner_2_last_name" onChange={props.handleChange}/></div>
+            </div>
+            <div className="row">
+              <div className="col-1"><label>Address</label></div>
+              <div className="col-2"><input type="text" name="owner_2_address" onChange={props.handleChange}/></div>
+            </div>
+            <h3>Owner 3</h3>
+            <div className="row">
+              <div className="col-1"><label>First Name</label></div>
+              <div className="col-2"><input type="text" name="owner_3_first_name" onChange={props.handleChange}/></div>
+            </div>
+            <div className="row">
+              <div className="col-1"><label>Last Name</label></div>
+              <div className="col-2"><input type="text" name="owner_3_last_name" onChange={props.handleChange}/></div>
+            </div>
+            <div className="row">
+              <div className="col-1"><label>Address</label></div>
+              <div className="col-2"><input type="text" name="owner_3_address" onChange={props.handleChange}/></div>
+            </div>
+            <h3>Owner 4</h3>
+            <div className="row">
+              <div className="col-1"><label>First Name</label></div>
+              <div className="col-2"><input type="text" name="owner_4_first_name" onChange={props.handleChange}/></div>
+            </div>
+            <div className="row">
+              <div className="col-1"><label>Last Name</label></div>
+              <div className="col-2"><input type="text" name="owner_4_last_name" onChange={props.handleChange}/></div>
+            </div>
+            <div className="row">
+              <div className="col-1"><label>Address</label></div>
+              <div className="col-2"><input type="text" name="owner_4_address" onChange={props.handleChange}/></div>
+            </div>
+            <div className="row accept">
+              <div className="col-1"><label>Accept Terms and Conditions:</label></div>
+              <div className="col-2"><input className="check-box" type="checkbox" name="isAccepted" checked={props.isAccepted} onChange={props.handleChange} /></div>
+            </div>
+            <div><input className="submit-button" type="submit" value="Update this greyhound" /></div>
+        </form>
     </div>
   )
-}
+} 
 export default UpdateGreyhoundForm
